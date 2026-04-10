c1f16dd444a6698f0e46321bc291797aeef63625
James de Oliveira <24202145@aluno.univesp.br>
2026-04-09 23:42:11 -0300

    Corrigindo busca por bairro

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-
 class PontoColeta(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
@@ -22,13 +21,12 @@ class PontoColeta(db.Model):
     cidade = db.Column(db.String(100), nullable=True)
     cep = db.Column(db.String(20), nullable=True)
     tipo_material = db.Column(db.String(200), nullable=False)
-
     telefone1 = db.Column(db.String(20))
     telefone2 = db.Column(db.String(20))
     observacoes = db.Column(db.Text)  # Text permite textos mais longos que String
-
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
+
 
     def __repr__(self):
         return f'<Ponto {self.nome}>'
@@ -58,20 +56,22 @@ def busca():
 
     # 2. Filtro por Localização (CEP, Bairro ou Cidade)
     if busca_texto:
-        # 1. Remove qualquer caractere que não seja número (hífens, pontos, espaços)
-        apenas_numeros = "".join(filter(str.isdigit, busca_texto))
+        # 1. Verifica se o usuário digitou algo que comece com número (provável CEP)
+        if busca_texto[0].isdigit():
+            apenas_numeros = "".join(filter(str.isdigit, busca_texto))
+            prefixo_cep = apenas_numeros[:3]
 
-        # 2. Pega apenas os 3 primeiros dígitos para a busca regional
-        # Se o usuário digitou menos de 5, o Python pega o que tiver
-        prefixo_cep = apenas_numeros[:3]
+            # Busca prioritária por CEP
+            query = query.filter(PontoColeta.cep.like(f"{prefixo_cep}%"))
 
-        query = query.filter(
-            or_(
-                PontoColeta.cep.like(f"{prefixo_cep}%"),  # Busca regional por CEP
-                PontoColeta.bairro.ilike(f"%{busca_texto}%"),
-                PontoColeta.cidade.ilike(f"%{busca_texto}%")
+        else:
+            # 2. Se digitou letras, busca por Bairro ou Cidade (Ignora o CEP aqui)
+            query = query.filter(
+                or_(
+                    PontoColeta.bairro.ilike(f"%{busca_texto}%"),
+                    PontoColeta.cidade.ilike(f"%{busca_texto}%")
+                )
             )
-        )
     resultados = query.all()
     return render_template('busca.html', resultados=resultados)
 
